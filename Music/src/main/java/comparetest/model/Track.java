874f5d75599f9aa880c4726a4936f766309025ea
gwmccort <gwmccort@gmail.com>
2013-03-15 16:58:43 -0500

    test

--- Music/src/main/java/comparetest/model/Track.java
+++ Music/src/main/java/comparetest/model/Track.java
@@ -1,6 +1,6 @@
 package comparetest.model;
 
-public class Track implements Comparable<Track>{
+public class Track implements Comparable<Track> {
 	String name;
 	String artist;
 	String album;
@@ -11,47 +11,52 @@ public class Track implements Comparable<Track>{
 		this.artist = artist;
 		this.album = album;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getArtist() {
 		return artist;
 	}
+
 	public void setArtist(String artist) {
 		this.artist = artist;
 	}
+
 	public String getAlbum() {
 		return album;
 	}
+
 	public void setAlbum(String album) {
 		this.album = album;
 	}
 
-	@Override
-	public int compareTo(Track t) {
+	 @Override
+	 public int compareTo(Track t) {
 
-//		int i = p1.firstName.compareTo(p2.firstName);
-//	    if (i != 0) return i;
-//
-//	    i = p2.lastName.compareTo(p2.lastName);
-//	    if (i != 0) return i;
-//
-//	    return p1.age - p2.age;
+	 // int i = p1.firstName.compareTo(p2.firstName);
+	 // if (i != 0) return i;
+	 //
+	 // i = p2.lastName.compareTo(p2.lastName);
+	 // if (i != 0) return i;
+	 //
+	 // return p1.age - p2.age;
 
 
-		int r;
-		r = name.compareTo(t.getName());
-		if (r !=0) return r;
+	 int r;
+	 r = name.compareTo(t.getName());
+	 if (r !=0) return r;
 
-		r = artist.compareTo(t.artist);
-		if (r !=0) return r;
+	 r = artist.compareTo(t.artist);
+	 if (r !=0) return r;
 
-		return album.compareTo(t.album);
-	}
-
+	 return album.compareTo(t.album);
+	 }
 
 	@Override
 	public String toString() {
@@ -61,57 +66,58 @@ public class Track implements Comparable<Track>{
 
 	/*
 	 * override hashCode & equals example from:
-	 * http://stackoverflow.com/questions/8180430/how-to-override-equals-method-in-java
+	 * http://stackoverflow.com/questions
+	 * /8180430/how-to-override-equals-method-in-java
 	 */
 
 	@Override
 	public int hashCode() {
-	    int hash = 3;
-	    hash = 53 * hash + (this.name != null ? this.name.hashCode() : 0);
-	    hash = 53 * hash + (this.artist != null ? this.artist.hashCode() : 0);
-	    hash = 53 * hash + (this.album != null ? this.album.hashCode() : 0);
+		int hash = 3;
+		hash = 53 * hash + (this.name != null ? this.name.hashCode() : 0);
+		hash = 53 * hash + (this.artist != null ? this.artist.hashCode() : 0);
+		hash = 53 * hash + (this.album != null ? this.album.hashCode() : 0);
 
-	    // int example
-	    //hash = 53 * hash + this.age;
+		// int example
+		// hash = 53 * hash + this.age;
 
-	    return hash;
+		return hash;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		   if (obj == null) {
-		        return false;
-		    }
+		if (obj == null) {
+			return false;
+		}
 
-		   // check if two obj's are same class
-//		    if (getClass() != obj.getClass()) {
-//		    	System.out.println("classes not equal");
-//		        return false;
-//		    }
+		// check if two obj's are same class
+		// if (getClass() != obj.getClass()) {
+		// System.out.println("classes not equal");
+		// return false;
+		// }
 
-		   // check if two obj's are subclasses of Track
-		    if (getClass().isAssignableFrom(Track.class) && obj.getClass().isAssignableFrom(Track.class)) {
-		    	System.out.println("classes not equal");
-		    	return false;
-		    }
+		// check if two obj's are subclasses of Track
+		if (getClass().isAssignableFrom(Track.class)
+				&& obj.getClass().isAssignableFrom(Track.class)) {
+			System.out.println("classes not equal");
+			return false;
+		}
 
-		    final Track t = (Track) obj;
-		    if ((name == null) ? (t.name != null) : !name.equals(t.name)) {
-		        return false;
-		    }
-		    if ((artist == null) ? (t.artist != null) : !artist.equals(t.artist)) {
-		        return false;
-		    }
-		    if ((album == null) ? (t.album != null) : !album.equals(t.album)) {
-		        return false;
-		    }
+		final Track t = (Track) obj;
+		if ((name == null) ? (t.name != null) : !name.equals(t.name)) {
+			return false;
+		}
+		if ((artist == null) ? (t.artist != null) : !artist.equals(t.artist)) {
+			return false;
+		}
+		if ((album == null) ? (t.album != null) : !album.equals(t.album)) {
+			return false;
+		}
 
-		    // int example
-//		    if (this.age != other.age) {
-//		        return false;
-//		    }
-		    return true;
+		// int example
+		// if (this.age != other.age) {
+		// return false;
+		// }
+		return true;
 	}
-
 
 }
